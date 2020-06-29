9c6a942c7329e2db9844d8ab76b7507b46b5aab2
Neko-Sato <hisatosi35350@gmail.com>
2020-06-29 20:54:03 +0900

    master

--- space/admin.py
+++ space/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Theme_board, Post
 
+admin.site.register(Theme_board)
 admin.site.register(Post)
--- space/models.py
+++ space/models.py
@@ -2,8 +2,16 @@ from django.db import models
 from django.conf import settings
 from django.utils import timezone
 
+class Theme_board(models.Model):
+    title = models.CharField(max_length=128)
+    x, y = models.FloatField(), models.FloatField()
+    datetime = models.DateTimeField(default=timezone.now)
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    Theme_board = models.ForeignKey(Theme_board, on_delete=models.CASCADE)
     contents = models.TextField()
     x, y = models.FloatField(), models.FloatField()
     datetime = models.DateTimeField(default=timezone.now)
