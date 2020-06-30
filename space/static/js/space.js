242aab119207332bf6ab6ca3836e1509adbc900b
unknown <180098>
2020-06-30 18:21:27 +0900

    master

--- space/static/js/space.js
+++ space/static/js/space.js
@@ -33,6 +33,31 @@ class communication {
 //以上関数定義
 //////
 
+//右上と左下の座標を送る
+const post_data = new communication('../postget/', function(){
+  console.log(this.responseText);
+});
+post_data.send(JSON.stringify({
+  TopLeft: {
+    x: XY[0] - window.innerWidth/2,
+    y: XY[1] - window.innerHeight/2,
+    },
+  BottomRight: {
+    x: XY[0] + window.innerWidth/2,
+    y: XY[1] + window.innerHeight/2,
+  },
+}));
+
+//受信した投稿を表示させる
+
+
+//更新があったら読み込み直す
+//座標を動かしたら
+//投稿も動かす
+//それで読み込み直す
+//画面サイズが変わったら読み込み直す
+
+/*
 //送るために
 const post_data = new communication('postget/', recv)
 
@@ -59,13 +84,13 @@ class MouseAction{
   }
   onmousedown_do() {
     this.MouseXY_temp = MouseXY;
-    space.style.cursor = "grabbing";
+    space.style.cursor = "move";
   }
   onmouseup_do() {
     var diff_XY = [this.MouseXY_temp[0] - MouseXY[0], this.MouseXY_temp[1] - MouseXY[1]];
     this.MouseXY_temp = [0, 0];
     XY = [XY[0] + diff_XY[0], XY[1] + diff_XY[1]];
-    space.style.cursor = "auto"; 
+    space.style.cursor = "auto";
   }
 }
 
@@ -77,8 +102,9 @@ window.onresize = window_load;
 window_load();
 
 function window_load() {
-  space.style.height = window.innerHeight + 'px'; 
-  space.style.Width = window.innerWidth + 'px';  
-  header.style.Width = window.innerWidth + 'px';  
-  hooder.style.Width = window.innerWidth + 'px'; 
-}  
+  space.style.height = window.innerHeight + 'px';
+  space.style.Width = window.innerWidth + 'px';
+  header.style.Width = window.innerWidth + 'px';
+  hooder.style.Width = window.innerWidth + 'px';
+}
+*/
--- space/urls.py
+++ space/urls.py
@@ -3,9 +3,8 @@ from . import views
 
 app_name = 'space'
 urlpatterns = [
-    path('', views.index), 
-    path('space/', views.space.as_view()),  
-    path('space/@<str:x><str:y>/', views.space.as_view()),  
+    path('', views.index),
+    path('space/@<str:x><str:y>/', views.space.as_view()),
     path('space/postget/', views.postget),
-    path('post/', views.post.as_view()), 
+    path('post/', views.post.as_view()),
 ]
